9756dd040dea35f6d22a3dc42d309be025e82714
unknown <choukhmane.khalid@gmail.com>
2020-06-15 19:36:35 +0100

    dashboard with static data

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
   // This is a String for the sake of an example.
   // You can use any type you want.
   final String data;
@@ -28,22 +28,58 @@ class SecondPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+
+
+  String presidentImage = 'assets/space-1.jpg';
+  String motPresident = 'A material design card. A card has slightly rounded corners and a shadow.'
+      'A card is a sheet of Material used to represent some related information, for example an album, a geographical location, '
+      'a meal, contact details, etc.This is what it looks like when run';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Routing App'),
+        title: Text(
+          'Mot du président',
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'Second Page',
-              style: TextStyle(fontSize: 50),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('$presidentImage'),
+                radius: 60.0,
+              ),
             ),
-            Text(
-              data,
-              style: TextStyle(fontSize: 20),
+            SizedBox(height: 20.0),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '$motPresident',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
